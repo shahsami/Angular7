6dd5de97a24b9a34811e9c2a6b8db4708cd8409f
SHAH MD. ABDUS SALAM MOHIUDDIN <shah.sami77@gmail.com>
2018-10-27 02:20:29 +0600

    created styles, components and services

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  h1Style: Boolean = false;
+
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+  }
+
+  firstClick() {
+    this.data.firstClick();
   }
 
 }
