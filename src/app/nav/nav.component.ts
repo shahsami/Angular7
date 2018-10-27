552413b407f01e33d482a79071bea187efe24fad
SHAH MD. ABDUS SALAM MOHIUDDIN <shah.sami77@gmail.com>
2018-10-27 07:55:43 +0600

    changed the name of the app and built deployable production build

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  appTitle: String = 'myapp';
+  appTitle: String = 'Ang7app';
 
   constructor() { }
 
